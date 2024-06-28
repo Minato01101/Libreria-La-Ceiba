f0f5b0d92948b3822d3dc696bef4613e2d100b5a
Minato01101 <SP23002ues.edu.sv>
2024-06-28 16:13:09 -0600

    Form registro de jugadores terminado

---
+++ LibreriaCeiba/Models/TOOLS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaCeiba.Models
+{
+    static class TOOLS
+    {
+        //Funcion para agragar imagen a un pictureBox
+        public static void SeleccionarImagen(PictureBox pictureBox)
+        {
+            var FileDialog = new OpenFileDialog();
+            var result = FileDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                pictureBox.Image = new Bitmap(FileDialog.FileName);
+                //MessageBox.Show(FileDialog.FileName);
+            }
+
+
+        }
+
+        //Funcion que convierte Imagen a binario
+        public static byte[] ConvertirImagenBinario(PictureBox pictureBox)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Guardar la imagen del PictureBox en el flujo de memoria
+                pictureBox.Image.Save(memoryStream, pictureBox.Image.RawFormat);
+
+                // Convertir la imagen a un array de bytes
+                return memoryStream.ToArray();
+            }
+        }
+
+        //Funcion que convierte binario a imagen
+        public static Image ConvertirBinarioImagen(byte[] byteArray)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(byteArray))
+            {
+                // Crear una imagen desde el array de bytes
+                Image imagen = Image.FromStream(memoryStream);
+                return imagen;
+            }
+        }
+    }
+}
