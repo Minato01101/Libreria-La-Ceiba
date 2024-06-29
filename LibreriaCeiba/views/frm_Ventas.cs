99165f0521c84854f5e707f7cf344fc9e7857d15
Minato01101 <SP23002ues.edu.sv>
2024-06-28 21:23:43 -0600

    form Ventas sin crear

---
+++ LibreriaCeiba/views/frm_Ventas.cs
@@ -0,0 +1,31 @@
+﻿using MaterialSkin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibreriaCeiba.views
+{
+    public partial class frm_Ventas : Form
+    {
+        public frm_Ventas()
+        {
+            InitializeComponent();
+
+            this.BackColor=Color.Black;
+            MaterialSkinManager theme = MaterialSkinManager.Instance;
+            theme.Theme  = MaterialSkinManager.Themes.DARK;
+            theme.ColorScheme = new ColorScheme(
+                Primary.Green300,
+                Primary.Green500,
+                Primary.Green800,
+                Accent.Green700,
+                TextShade.BLACK);
+        }
+    }
+}
